5d52b1a51ab56fd6262f60841c9bedb5138cd0a8
Fawwaz <fawwaz.chowdhury@googlemail.com>
2024-12-03 02:17:23 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import scrollbarHide from "tailwind-scrollbar-hide";
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
